bc8d3e53805fa440ac315f2dfbd2aadbb129ce2e
Leon Mlodzian <leon.mlodzian@protonmail.com>
2021-08-01 13:17:06 +0200

    init

---
+++ UIUCTF2021/CEO.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+
+common_words = []
+num_words_in_pw = 1
+
+
+with open('/Users/leonmlodzian/Downloads/3000+ Common English Words.txt', 'r') as f:
+    for line in f.readlines():
+        common_words.append(line[:-1])
+
+
+def write_flag(f, perm):
+    flag = ''
+    for word in perm:
+        flag += word + '_'
+    flag = flag[:-1] + '\n'
+    f.write(flag)
+
+
+with open('dictionary.txt', 'w') as f:
+    perms = permutations(common_words[3:], num_words_in_pw)
+    for perm in perms:
+        write_flag(f, perm)
+
+
